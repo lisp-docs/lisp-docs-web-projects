a945c4e504c48f78b987129b7b79ad32a83297b6
Daniel Nussenbaum <daninus14@gmail.com>
2023-12-07 16:02:59 +0200

    saving progress basic structure

--- apps/lisp-docs-web-projects/src/app/app.tsx
+++ apps/lisp-docs-web-projects/src/app/app.tsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { LispDocsUtils } from '@lisp-docs/utils';
 
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="lisp-docs-web-projects" />
+      {/* <NxWelcome title="lisp-docs-web-projects" /> */}
+      <p>Hello world!</p>
+      <LispDocsUtils></LispDocsUtils>
     </div>
   );
 }
