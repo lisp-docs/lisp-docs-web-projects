2e0d21775ccc2471c20b250f5ae0f586039efd87
Daniel Nussenbaum <daninus14@gmail.com>
2023-12-21 17:19:06 +0200

    trying to include json in export

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,4 +1,8 @@
+import * as dictionaryJson from './lib/definition-tooltips/dictionary.json';
+import * as glossaryJson from './lib/definition-tooltips/glossary.json';
 export * from './lib/definition-tooltips/definition-tooltips';
 export * from './lib/definition-tooltips/reference-link';
 export * from './lib/definition-tooltips/reference-aid';
 export * from './lib/Utils';
+export { dictionaryJson }
+export { glossaryJson }
